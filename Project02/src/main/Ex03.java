4c7086012b5e8b496a8eba2669273b2437407838
yoyt777 <yoyt777@gmail.com>
2023-10-16 21:36:45 +0900

    aa

--- Project02/src/main/Ex03.java
+++ Project02/src/main/Ex03.java
@@ -4,17 +4,17 @@ package main;
  * 변수 이름 정하기
  * */
 public class Ex03 {
-		public static void main(String[] args) {
-			//가격
-			int price; //소문자로 시작한다
-			//학생수
-			int numberOfStudent; //여러단어를 조합할 경우 중간에 등장하는 단어는 대문자로 시작한다 (카멜케이스)
-			//사용자이름
-			String userName;
-			//예금
-			int saveMoney;
-			
-			/* 다른언어에서 사용하는 표기법 (ex.파이썬)*/
-			int number_of_student; //언더바
-		}
+	public static void main(String[] args) {
+		// 가격
+		int price; // 소문자로 시작한다
+		// 학생수
+		int numberOfStudent; // 여러단어를 조합할 경우 중간에 등장하는 단어는 대문자로 시작한다 (카멜케이스)
+		// 사용자이름
+		String userName;
+		// 예금
+		int saveMoney;
+
+		/* 다른언어에서 사용하는 표기법 (ex.파이썬) */
+		int number_of_student; // 언더바
+	}
 }
