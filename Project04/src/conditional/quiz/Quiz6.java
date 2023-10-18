4f90ed8f7b1a91e1ba7423b48d0f5b4d2dd456f2
yoyt777 <yoyt777@gmail.com>
2023-10-18 19:15:44 +0900

    aa

--- Project04/src/conditional/quiz/Quiz6.java
+++ Project04/src/conditional/quiz/Quiz6.java
@@ -1,27 +1,30 @@
 package conditional.quiz;
 
 /*
- * switch문을 이용해서 승객의 연령에 따라 버스 요금을 출력하세요
- * String타입 변수를 선언하고 승객의 연령을 입력하고, 조건으로 사용하세요
- * 미성년자 - 1000원
- * 성인 - 1500원
- * 노인 - 무료
+ * <elseif문>
+ * 학생의 점수를 저장할 변수를 선언하고 0~99 사이의 값을 저장하세요.
+ * 
+ * 점수가 90~99 이면 "수"
+ * 점수가 80~89 이면 "우"
+ * 점수가 70~79 이면 "미"
+ * 그외는 "양"을 출력하세요.
  * */
+
 public class Quiz6 {
 
 	public static void main(String[] args) {
-		String age = "성인"; // 미성년자, 성인, 노인
+		
+		int score = 50; //학생의 점수를 저장할 변수를 선언한다
 
-		switch (age) {
-		case "미성년자":
-			System.out.println("요금은 1000원입니다");
-			break;
-		case "성인":
-			System.out.println("요금은 1500원입니다");
-			break;
-		case "노인":
-			System.out.println("요금은 무료입니다");
-			break;	
+		if(score >= 90 && score<=99) { //학생의 점수가 90점 이상이면
+			System.out.println("수");
+		}else if(score >= 80) { //점수가 80이상이면
+			System.out.println("우");
+		}else if(score >= 70) { //점수가 70이상이면
+			System.out.println("미");
+		}else { //모두 아니라면
+			System.out.println("양");
 		}
+
 	}
 }
