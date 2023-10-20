485a0edc7e975a49241d7395a0a76b1f30b50ffb
205-T <205-T@sam205>
2023-10-20 13:11:39 +0900

    코드 추가

--- Project04/src/loop/quiz/Quiz1.java
+++ Project04/src/loop/quiz/Quiz1.java
@@ -7,16 +7,28 @@ package loop.quiz;
 public class Quiz1 {
 
 	public static void main(String[] args) {
-
-		int num = 1; //더할 숫자
-		int sum = 0; //합계를 저장할 변수
+		int sum = 0;
 		
-		while(num <= 10){ //조건을 만족하는 동안 총 10번 코드를 실행한다
-			sum = sum + num; // num을 1씩 증가시켜서 sum에 계속 더한다
-//			System.out.println("num: " + num + ",sum: " + sum); //중간과정
-			num++; //1,2,3,4,5,6,7,8,9,10
-		}
+		sum = sum + 1;
+		sum = sum + 2;
+		sum = sum + 3;
+		sum = sum + 4;
+		sum = sum + 5;
+		sum = sum + 6;
+		sum = sum + 7;
+		sum = sum + 8;
+		sum = sum + 9;
+		sum = sum + 10;
 		System.out.println("1부터10까지의 합은 " + sum + "입니다.");
+		
+//		int num = 1; //더할 숫자
+//		int sum = 0; //합계를 저장할 변수
+//		
+//		while(num <= 10){ //조건을 만족하는 동안 총 10번 코드를 실행한다
+//			sum = sum + num; // num을 1씩 증가시켜서 sum에 계속 더한다
+//			num++; //1,2,3,4,5,6,7,8,9,10
+//		}
+//		System.out.println("1부터10까지의 합은 " + sum + "입니다.");
 	}
 }
 
