612306af4a917ac5ab03d483362c4b7030507d76
205-T <205-T@sam205>
2023-10-23 13:44:12 +0900

    aa

--- Project04/src/loop/quiz/Quiz9.java
+++ Project04/src/loop/quiz/Quiz9.java
@@ -1,6 +1,5 @@
 package loop.quiz;
 
-
 //*****
 //*****
 //*****
@@ -9,17 +8,24 @@ package loop.quiz;
 * */
 public class Quiz9 {
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
+
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
 		
-		for(int i = 1; i <=3; i++){
-			
-			for(int j = 1; j <=5; j++){
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("*****");
+		}
+
+		for (int i = 1; i <= 3; i++) {
+
+			for (int j = 1; j <= 5; j++) {
 				System.out.print('*');
 			}
-			
+
 			System.out.println();
 		}
-		
+
 	}
 }
-
