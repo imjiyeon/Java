2c3469f8e3b1993837dff060f5e8395465073038
yoyt777 <yoyt777@gmail.com>
2023-10-23 17:44:11 +0900

    주석 수정

--- Project05/src/object/Person.java
+++ Project05/src/object/Person.java
@@ -1,10 +1,10 @@
 package object;
 
 /*
- * 2. 사람 클래스 만들기
+ * 2. 사람을 정의하는 클래스 만들기
  * */
 public class Person {
-	//사람의 속성을 멤버 변수로 선언한다 
+	//사람의 속성을 멤버 변수로 선언
 	
 	String name; //이름 (둘리)
 	int height; //키 (170)
--- Project05/src/object/Student.java
+++ Project05/src/object/Student.java
@@ -4,15 +4,18 @@ package object;
  * 1. 학생 클래스 만들기
  * */
 public class Student {
-	//학생의 속성을 멤버 변수로 선언한다
+	//학생의 속성을 멤버 변수로 선언
 	int studentID; //학번 (1001)
 	String studentName; //이름 (둘리)
 	int grade; //학년 (3)
 	String address; //주소 (인천 남동구 ~)
-	
-	//학생의 정보를 출력하는 기능을 구현한다
-	public void showStudentInfo() {
-		System.out.println(studentName + "," + address); //이름, 주소 출력
+
+	//학생의 기능을 메소드로 선언
+	public void goSchool() {
+		System.out.println("학교에 간다");
 	}
 
+	public void study() {
+		System.out.println("공부를 한다");
+	}
 }
