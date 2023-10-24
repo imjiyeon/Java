ddd43cd970769db0e5adb48cd1bc4601ed05db90
205-T <205-T@sam205>
2023-10-24 11:28:33 +0900

    자료형 수정

--- Project05/src/object/quiz/Movie.java
+++ Project05/src/object/quiz/Movie.java
@@ -1,5 +1,7 @@
 package object.quiz;
 
+import java.util.Date;
+
 /*
  * 영화(Movie)을 정의하는 클래스를 만드세요.
  * 속성: 제목, 가격, 감독, 상영시간
@@ -8,6 +10,6 @@ public class Movie {
 	  String title; //제목 (영웅)
 	  int price; //가격 (15000)
 	  String author; //감독 (윤제균)
-	  float time; //상영시간 (2.0)
+	  Date time; //상영시간 (2.0)
 }
 
