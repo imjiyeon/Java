1e17a4c7fd1b0aee581f4358564bb32ee32ea0f0
205-T <205-T@sam205>
2023-10-24 11:11:36 +0900

    주석 수정

--- Project05/src/object/quiz/Member.java
+++ Project05/src/object/quiz/Member.java
@@ -5,8 +5,8 @@ package object.quiz;
  * 속성: 이름, 아이디, 패스워드, 나이
  * */
 public class Member {
-	String name; //이름
-	String id; //아이디
-	String password; //패스워드
-	int age; //나이
+	String name; //이름 (둘리)
+	String id; //아이디 (jiyeon)
+	String password; //패스워드 (asdw1234)
+	int age; //나이 (20)
 }
