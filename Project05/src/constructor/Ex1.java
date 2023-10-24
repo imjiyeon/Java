bd3a8e326e3373d41bbe4a57b4ea19fbb65367bd
yoyt777 <yoyt777@gmail.com>
2023-10-24 20:04:58 +0900

    퀴즈 추가

--- Project05/src/constructor/Ex1.java
+++ Project05/src/constructor/Ex1.java
@@ -1,14 +1,14 @@
 package constructor;
 
 /*
- * 생성자 만들고 테스트하기
+ * 생성자 만들고 객체 생성하기
  * */
 public class Ex1 {
 
 	public static void main(String[] args) {
-		Person1 personA = new Person1(); //컴파일러가 자동으로 제공하는 디폴트 생성자 사용
+		Person1 personA = new Person1(); //디폴트 생성자 사용하여 인스턴스 생성
 		
-		//Person2 personB = new Person2(); //에러남. 클래스에 생성자를 직접 만들었기 때문에 컴파일러가 디폴트생성자를 만들지 않았음.
+		//Person2 personB = new Person2(); //에러남. 클래스에 생성자를 직접 만들었기 때문에 컴파일러가 디폴트생성자를 만들지 않았음
 		Person2 personC = new Person2("둘리"); //인스턴스 생성과 동시에 이름 초기화
 	}
 }
@@ -19,7 +19,7 @@ class Person1 {
 	int weight;
 }
 
-// 생성자 만들기
+// 사용자 정의 생성자 만들기
 class Person2 {
 	String name;
 	int height;
--- Project05/src/constructor/Ex2.java
+++ Project05/src/constructor/Ex2.java
@@ -1,27 +1,27 @@
 package constructor;
 
 /*
- * 생성자 오버로드
+ * 사용자 정의 생성자 만들고 객체 생성하기
  * */
 public class Ex2 {
 
 	public static void main(String[] args) {
 
-		// 디폴트 생성자 메소드를 사용하여 인스턴스 생성
+		// 첫번째 생성자 메소드를 사용하여 인스턴스 생성
 		Person person1 = new Person();  
 		person1.name = "둘리"; // 인스턴스 생성 후 멤버변수 값을 따로 초기화
 		person1.weight = 80;
 		person1.height = 180;
 
-		// 이름을 입력받는 생성자를 사용하여 인스턴스 생성
+		// 두번째 생성자를 사용하여 인스턴스 생성
 		Person person2 = new Person("또치"); // 인스턴스 생성과 동시에 이름 초기화
 		person2.weight = 50;
 		person2.height = 160;
 
-		// 모든 정보를 입력받는 생성자를 사용하여 인스턴스 생성
+		// 세번째 생성자를 사용하여 인스턴스 생성
 		Person person3 = new Person("도우너", 170, 60); // 인스턴스 생성과 동시에 모든 멤버변수 초기화
 		
-		//이렇게 매개변수가 있는 생성자를 사용하면 값초기화 하기 편리하다
+		//사용자 정의 생성자를 사용하면 값을 편하게 입력할 수 있다
 	}
 }
 
@@ -31,7 +31,7 @@ class Person {
 	int weight;
 
 	// 디폴트 생성자
-	public Person() { // 생성자가 하나도 없으면 자바 컴파일러가 자동으로 디폴트 생성자를 만든다
+	public Person() { // 생성자가 하나도 없으면 컴파일러가 자동으로 디폴트 생성자를 만든다
 	}
 
 	// 이름을 매개변수로 받아서 초기화하는 생성자
