af9db379772cf4813591ff8684bd4d9feed0e3a0
205-T <205-T@sam205>
2023-10-25 11:02:42 +0900

    문제 수정

--- Project05/src/method/quiz/Quiz2.java
+++ Project05/src/method/quiz/Quiz2.java
@@ -1,29 +1,35 @@
 package method.quiz;
 
 /* 
- * 두 수를 입력받아, 둘 중에 더 큰 수를 출력하는 함수를 만들고 호출하세요.
+ * 두 수를 비교하는 함수 만들고 호출하세요.
+ * 둘 중에 더 큰 수를 구하고 반환하세요.
  * 예시) 10,5 => 10
  * */
 public class Quiz2 {
 
 	//두수를 비교하는 함수 선언
-	public static void max(int n1, int n2){
-		if ( n1 <= n2) {
-			System.out.println(n2);
+	public static int max(int n1, int n2){
+		if ( n1 > n2) {
+			return n1;
 		} else {
-			System.out.println(n1);
+			return n2;
 		}	
+		//모든 경우에 return을 해야함
 	}
 	
 	public static void main(String[] args) {
-		max(10,5); //함수 호출
-		max(2,7); 
-		max(5,5); 
+		int result1 = max(10,5); //함수 호출
+		int result2 = max(2,7); 
+		int result3 = max(5,5); 
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
 	}
 }
 
 
 //문제풀이
 //매개변수는 int형 변수 2개
-//리턴값x
-//두수를 비교하여 더 큰 값 출력
+//리턴값o
+//두수를 비교하여 더 큰 값 반환
