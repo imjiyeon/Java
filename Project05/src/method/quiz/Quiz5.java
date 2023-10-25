8eca649d0d22c0ddcb3a740bec3070b78be454d6
205-T <205-T@sam205>
2023-10-25 10:26:13 +0900

    주석 추가

--- Project05/src/method/quiz/Quiz5.java
+++ Project05/src/method/quiz/Quiz5.java
@@ -5,6 +5,7 @@ package method.quiz;
  * 두 수를 입력받아 첫번째수에서 두번째수를 나눈 몫을 출력하세요.
  * 
  * 단, 두번째 수가 0이면 "나누는 수는 0이 될수 없습니다"를 출력하고 함수를 종료하세요.
+ * 예시) 10,0 -> 나누는 수는 0이 될 수 없습니다
  * */
 public class Quiz5 {
 
@@ -16,13 +17,12 @@ public class Quiz5 {
 			return; // 함수를 강제로 종료한다
 		} 
 		result = n1/n2;
-		System.out.println(n1 + " / " + n2 + " = " + result + "입니다");
+		System.out.println(n1 + "/" + n2 + " = " + result);
 	}
 
 	public static void main(String[] args) {
-		int num1 = 10;
-		int num2 = 2;
-		divide(num1, num2);	//함수 호출
+		divide(10, 2);	//함수 호출
+		divide(10, 0);
 	}
 
 }
