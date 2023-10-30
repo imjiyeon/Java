368366abc15185d65091d5dea687887956454746
205-T <205-T@sam205>
2023-10-30 10:58:46 +0900

    문제 추가

--- Project05/src/hiding/quiz/Quiz1.java
+++ Project05/src/hiding/quiz/Quiz1.java
@@ -1,50 +1,27 @@
 package hiding.quiz;
 
 /*
- * Mydate 클래스의 setMonth 메소드에 다음과 같이 조건을 추가하세요.
- * 1~12월이 들어오면 값을 변경하고, 
- * 그외의 값이 들어오면 "잘못된 월입니다" 메세지를 출력합니다.
+ * 책(Book)을 정의하는 클래스를 만드세요.
+ * 속성: 제목, 가격, 출판사, 페이지수
+ * 기능: 각 멤버변수의 getter와 setter
+ * 
+ * Book 클래스를 사용하여 다음 정보를 가지는 두 개의 인스턴스를 생성하세요.
+ * 이때, 책의정보는 setter를 사용해서 변경하세요.
+ * (자바프로그래밍, 20000, 한빛컴퍼니, 300)
+ * (스프링, 15000, 한빛컴퍼니, 500)
+ * 모든 인스턴스의 정보를 출력하세요.
+ * 이때, 책의 정보는 getter를 사용해서 출력하세요.
  * */
 public class Quiz1 {
-
 	public static void main(String[] args) {
-		MyDate date = new MyDate();
-		date.setMonth(2);
-		System.out.println("현재는 " + date.getMonth() + "월 입니다");
-
-		date.setMonth(13);
-		System.out.println("현재는 " + date.getMonth() + "월 입니다");
+//		Book book1 = new Book();
+//		book1.set
 	}
 }
 
-class MyDate {
-
-	private int month; // 월
-	private int day; // 일
-
-	public int getMonth() {
-		return month;
-	}
-
-	public void setMonth(int month) {
-		if (month < 1 || month > 12) {
-			System.out.println(month + "월은 잘못된 월입니다.");
-			return; //함수종료
-		} 
-		this.month = month;
-	}
-
-	public int getDay() {
-		return day;
-	}
-
-	public void setDay(int day) {
-		if (month == 2) {
-			if (day < 1 || day > 28) {
-				System.out.println("2월에는 " + day + " 일이 없습니다!!");
-				return;
-			}
-		}
-	}
-
+class Book {
+	String title; //제목
+	int price; //가격
+	String company; //출판사
+	int page; //페이지수
 }
