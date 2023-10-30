77d5267b16e7ac3508c7634ff88558a11ce9a14c
205-T <205-T@sam205>
2023-10-30 09:17:31 +0900

    ㅁㅁ

--- Project05/src/hiding/test2/Test.java
+++ Project05/src/hiding/test2/Test.java
@@ -1,13 +1,13 @@
 package hiding.test2;
 
-import hiding.test1.A; //다른패키지에 있는 클래스를 사용할 때는 import문 선언해야함
-//import hiding.test1.B;
+//다른패키지에 있는 클래스를 사용할 때는 import문 선언해야함
+import hiding.test1.A;
 
 public class Test {
 
 	public static void main(String[] args) {
 		A a = new A(); //외부 패키지에서 사용가능
-//		B b = new B(); //외부 클래스에서 사용불가
+		//B b = new B(); //외부 패키지에서 사용불가
 	}
 
 }
