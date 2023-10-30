0d38fe30a2e7be8c13ed1d49ce8a821fa56ad9ac
205-T <205-T@sam205>
2023-10-30 11:23:25 +0900

    문제 수정

--- Project05/src/hiding/quiz/Quiz2.java
+++ Project05/src/hiding/quiz/Quiz2.java
@@ -1,50 +1,59 @@
 package hiding.quiz;
 
 /*
- * Mydate 클래스의 setMonth 메소드에 다음과 같이 조건을 추가하세요.
- * 1~12월이 들어오면 값을 변경하고, 
- * 그외의 값이 들어오면 "잘못된 월입니다" 메세지를 출력합니다.
+ * 자동차(Car)을 정의하는 클래스를 만드세요.
+ * 속성: 제조사, 모델명, 색, 최고속도
+ * 기능: 각 멤버변수의 getter와 setter
+ * 
+ * 자동차 클래스를 사용하여 다음 정보를 가지는 인스턴스를 생성하세요.
+ * 이때, 자동차의정보는 setter를 사용해서 변경하세요.
+ * (현대, 소나타, 흰색, 200)
+ * 자동차의 정보를 출력하세요.
+ * 이때, 자동차의 정보는 getter를 사용해서 출력하세요.
  * */
 public class Quiz2 {
-
 	public static void main(String[] args) {
-		MyDate date = new MyDate();
-		date.setMonth(2);
-		System.out.println("현재는 " + date.getMonth() + "월 입니다");
-
-		date.setMonth(13);
-		System.out.println("현재는 " + date.getMonth() + "월 입니다");
+		Car car = new Car();
+		car.setCompany("현대");
+		car.setModel("소나타");
+		car.setColor("흰색");
+		car.setMaxSpeed(200);
+		System.out.println("제조사:"+car.getCompany());
+		System.out.println("모델:"+car.getModel());
+		System.out.println("색:"+car.getColor());
+		System.out.println("최고속도:"+car.getMaxSpeed());
 	}
 }
 
-class MyDate {
-
-	private int month; // 월
-	private int day; // 일
-
-	public int getMonth() {
-		return month;
+class Car {
+	private String company; //제조사
+	private String model; //모델
+	private String color; //색
+	private int maxSpeed; //최고속도
+	
+	public String getCompany() {
+		return company;
 	}
-
-	public void setMonth(int month) {
-		if (month < 1 || month > 12) {
-			System.out.println(month + "월은 잘못된 월입니다.");
-			return; //함수종료
-		} 
-		this.month = month;
+	public void setCompany(String company) {
+		this.company = company;
 	}
-
-	public int getDay() {
-		return day;
+	public String getModel() {
+		return model;
 	}
-
-	public void setDay(int day) {
-		if (month == 2) {
-			if (day < 1 || day > 28) {
-				System.out.println("2월에는 " + day + " 일이 없습니다!!");
-				return;
-			}
-		}
+	public void setModel(String model) {
+		this.model = model;
 	}
-
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+	
 }
