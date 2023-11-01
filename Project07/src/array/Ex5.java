c795fb22b35ce4cce714ea037b5d0b25f2cbed6f
205-T <205-T@sam205>
2023-11-01 09:50:59 +0900

    aa

--- Project07/src/array/Ex5.java
+++ Project07/src/array/Ex5.java
@@ -12,8 +12,8 @@ public class Ex5 {
 
 		// 람다식을 사용한 반복문은 간결한 코드를 작성할 수 있음
 		// 람다식 포문을 사용하면 배열을 순회하며 각 요소를 처리함
-		for (int i : arr) { // (배열에서 꺼낸 값을 저장할 변수 : 배열)
-			System.out.print(i + " ");
+		for (int value : arr) { // (배열에서 꺼낸 값을 저장할 변수 : 배열)
+			System.out.print(value + " ");
 		}
 		System.out.println();
 		for (int i = 0; i < 10; i++) {
