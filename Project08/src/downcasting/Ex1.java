49d23f2df915332fd7c0f609a20f8af053ffd4f8
yoyt777 <yoyt777@gmail.com>
2023-11-05 09:55:08 +0900

    ㅁㅁ

--- Project08/src/downcasting/Ex1.java
+++ Project08/src/downcasting/Ex1.java
@@ -1,43 +1,31 @@
 package downcasting;
 
-import java.util.ArrayList;
-
 /*
- * instanceof로 원래 인스턴스형 확인 후 다운캐스팅하기
+ * 형변환된 변수를 다운캐스팅하기
  **/
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// 사람,호랑이,독수리를 담을 배열 생성
-		ArrayList<Animal> aniList = new ArrayList<Animal>(); // 베열의 자료형은 Animal로 지정
-		aniList.add(new Human()); // 리스트에 사람 추가
-		aniList.add(new Tiger()); // 리스트에 호랑이 추가
-		aniList.add(new Eagle()); // 리스트에 독수리 추가
 
-		for (int i = 0; i < aniList.size(); i++) {
-			Animal ani = aniList.get(i); // 배열의 요소를 하나씩 Animal형으로 가져옴
-			if (ani instanceof Human) { // 실제 인스턴스의 타입이 Human형이면
-				Human h = (Human) ani; // Human으로 다운캐스팅
-				h.readBook();
-			} else if (ani instanceof Tiger) { // 실제 인스턴스의 타입이 Tiger형이면
-				Tiger t = (Tiger) ani; // Tiger으로 다운캐스팅
-				t.hunting();
-			} else if (ani instanceof Eagle) { // 실제 인스턴스의 타입이 Eagle형이면
-				Eagle e = (Eagle) ani; // Eagle으로 다운캐스팅
-				e.flying();
-			}
+		Animal animal = new Human(); // animal변수를 형변환 시킴
+
+		if (animal instanceof Human) { // 실제 인스턴스의 타입이 Human형인지 확인
+			Human human = (Human) animal; // Human형 변수로 다운캐스팅
+			human.readBook(); // 객체가 가지고 있는 readBook기능을 사용할 수 있음
 		}
 	}
 
 }
 
-class Animal { // 상위클래스
+//동물 클래스
+class Animal {
 	public void move() {
 		System.out.println("동물이 움직입니다.");
 	}
 }
 
-class Human extends Animal { // Animal을 상속받은 Human클래스
+//사람 클래스
+class Human extends Animal { // 동물클래스 상속받기
 	public void move() { // 메소드 재정의
 		System.out.println("사람이 두 발로 걷습니다. ");
 	}
@@ -47,7 +35,8 @@ class Human extends Animal { // Animal을 상속받은 Human클래스
 	}
 }
 
-class Tiger extends Animal { // Animal을 상속받은 Tiger클래스
+//호랑이 클래스
+class Tiger extends Animal {
 	public void move() {
 		System.out.println("호랑이가 네 발로 뜁니다. ");
 	}
@@ -57,7 +46,8 @@ class Tiger extends Animal { // Animal을 상속받은 Tiger클래스
 	}
 }
 
-class Eagle extends Animal { // Animal을 상속받은 Eagle클래스
+//독수리 클래스
+class Eagle extends Animal {
 	public void move() {
 		System.out.println("독수리가 하늘을 납니다 ");
 	}
--- Project08/src/downcasting/Quiz1.java
+++ Project08/src/downcasting/Quiz1.java
@@ -2,18 +2,26 @@ package downcasting;
 
 import java.util.ArrayList;
 
+/*
+ * 사람, 학생, 회사원 클래스를 정의하세요.
+ * Person형 리스트를 생성하고 학생과 회사원을 저장하세요.
+ * 리스트에 저장된 사람을 모두 꺼내서, 메소드를 호출하세요.
+ * */
 public class Quiz1 {
 	public static void main(String[] args) {
+		
 		ArrayList<Person> list = new ArrayList<Person>();
 		list.add(new Student("둘리")); //학생 추가
 		list.add(new Employee("또치")); //회사원 추가
 
 		for (Person person : list) {
 			
-			person.eat(); //Person이 물려준 eat메소드 호출
+			person.eat(); //부모가 물려준 eat메소드 호출
+			
 			if (person instanceof Student) { //인스턴스의 타입이 Student형이라면
 				Student student = (Student) person; //Student형으로 다운캐스팅
 				student.study();
+				
 			} else if (person instanceof Employee) {
 				Employee employee = (Employee) person;
 				employee.work();
