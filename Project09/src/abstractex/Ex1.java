e9777782c29712472ad3635bf5a13b3ea206cbed
yoyt777 <yoyt777@gmail.com>
2023-11-05 13:33:27 +0900

    ㅁㅁ

--- Project09/src/abstractex/Ex1.java
+++ Project09/src/abstractex/Ex1.java
@@ -1,36 +1,31 @@
 package abstractex;
 
 /*
- * 추상클래스 구현하기, 상속받기
+ * 추상클래스 만들고 상속받기
  * */
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// Computer computer = new Computer(); //에러발생, 추상클래스로는 인스턴스를 생성할수없다
-		Computer desktop = new DeskTop();
-		Computer notebook = new NoteBook();
+
+		// Computer computer = new Computer(); //에러남. 추상클래스로는 인스턴스를 생성할수없다
+		DeskTop desktop = new DeskTop();
+		NoteBook notebook = new NoteBook();
 
 		desktop.turnOn();
 		desktop.turnOff();
 		desktop.display();
 		desktop.typing();
-		System.out.println();
+
 		notebook.turnOn();
 		notebook.turnOff();
 		notebook.display();
 		notebook.typing();
-
 	}
 }
 
-abstract class Computer { // 추상메소드가 있으면 추상클래스로 선언해야함
-
-	// 바디가없는 추상메소드 선언
-	public abstract void display();
-
-	public abstract void typing();
-
-	// 일반메소드 (공통기능)
+//컴퓨터 클래스
+abstract class Computer { // 추상클래스 만들기
+	// 공통기능은 일반메소드로 선언
 	public void turnOn() {
 		System.out.println("전원을 켭니다.");
 	}
@@ -38,31 +33,38 @@ abstract class Computer { // 추상메소드가 있으면 추상클래스로 선
 	public void turnOff() {
 		System.out.println("전원을 끕니다.");
 	}
+
+	// 추상메소드
+	public abstract void display(); // 바디없음
+
+	public abstract void typing();
 }
 
-class DeskTop extends Computer { //구현되지 않은 메소드가 있으면 에러남
+//데스크톱 클래스
+class DeskTop extends Computer { // 컴퓨터클래스 상속받기. 구현되지 않은 메소드가 있으면 에러남
 
 	@Override
-	public void display() { //추상클래스의 구현부 작성
+	public void display() { // 추상클래스 구현
 		System.out.println("DeskTop Display()");
 	}
 
 	@Override
-	public void typing() {
+	public void typing() { // 추상클래스 구현
 		System.out.println("DeskTop Typing()");
 	}
 
 }
 
-class NoteBook extends Computer { //구현되지 않은 메소드가 있으면 에러남
+//노트북 클래스
+class NoteBook extends Computer { // 컴퓨터클래스 상속받기
 
 	@Override
-	public void display() { //추상클래스의 구현부 작성
+	public void display() { // 추상클래스 구현
 		System.out.println("NoteBook Display()");
 	}
 
 	@Override
-	public void typing() {
+	public void typing() { // 추상클래스 구현
 		System.out.println("NoteBook Typing()");
 	}
 
--- Project09/src/abstractex/Ex2.java
+++ Project09/src/abstractex/Ex2.java
@@ -1,54 +1,52 @@
 package abstractex;
 
 /*
- * 추상클래스 구현하기, 상속받기
+ * 추상클래스 만들고 상속받기
  * */
 public class Ex2 {
 	public static void main(String[] args) {
-		Dog dog = new Dog();
-		Cat cat = new Cat();
-		dog.sound();
-		cat.sound();
-		System.out.println("-----");
+
+		Animal animal1 = new Dog(); // 부모로 형변환
+		animal1.sound(); //재정의된 메소드가 호출됨
 		
-		//부모로 형변환
-		Animal animal1 = new Dog();
-		animal1.sound();
 		Animal animal2 = new Cat();
 		animal2.sound();
 	}
 }
 
+//동물 클래스
 abstract class Animal {
-	public String kind; //동물의 종
-	
-	//breath()메소드는 동물들이 공통으로 사용하므로 일반 메소드로 선언
+	String kind; // 동물의 종
+
+	// 공통기능은 일반메소드로 선언
 	public void breathe() {
 		System.out.println("숨을 쉽니다");
 	}
 
-	//sound()메소드는 동물마다 고유의 소리를 내도록 추상메소드로 선언
+	// 동물마다 고유의 소리를 내므로 추상메소드로 선언
 	public abstract void sound();
 }
 
-class Cat extends Animal {
+//고양이 클래스
+class Cat extends Animal { // 상속받기
 	public Cat() {
 		this.kind = "포유류";
 	}
 
 	@Override
-	public void sound() {
+	public void sound() { // 물려받은 추상메소드 구현하기
 		System.out.println("야옹");
 	}
 }
 
-class Dog extends Animal {
+// 강아지 클래스
+class Dog extends Animal { // 상속받기
 	public Dog() {
 		this.kind = "포유류";
 	}
 
 	@Override
-	public void sound() {
+	public void sound() { // 물려받은 추상메소드 구현하기
 		System.out.println("멍멍");
 	}
 }
