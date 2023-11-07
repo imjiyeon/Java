b6676ee9852156e28bc4c576a2a5933d36ccb949
205-T <205-T@sam205>
2023-11-07 12:08:05 +0900

    aaa

--- Project10/src/ex/Ex2.java
+++ Project10/src/ex/Ex2.java
@@ -8,9 +8,11 @@ public class Ex2 {
 	public static void main(String[] args) {
 		
 		Calc calc = new Calculator(); // 부모타입으로 변수를 형변환시킴
+
+		calc.add(2, 3);
 		
-		System.out.println(calc.add(2, 3));
-		//calc.showInfo(); // 에러남. 자식클래스에서 추가한 기능은 사용할 수 없음
+		//calc.showInfo(); // 에러남.
+		
 	}
 
 }
