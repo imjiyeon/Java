cc690125877993342c82effa197867efa150c44c
imjiyeon <yoyt12@naver.com>
2023-12-06 08:46:52 +0900

    aa

--- Project11/src/object/Ex2.java
+++ Project11/src/object/Ex2.java
@@ -1,16 +1,16 @@
 package object;
 
 /*
- * Object 클래스에서 상속받는 메소드 보기
+ * Object 클래스에서 상속받은 메소드 알아보기
  * */
 public class Ex2 {
 
 	public static void main(String[] args) {
 		A a = new A();
-		a.toString(); //참조변수로 상속받은 Object 메소드 확인		
+		a.toString(); //object에서 상속받은 메소드 사용하기
 	}
 }
 
-class A {
-	//오버라이드 메뉴에서 상속받은 Object 메소드 확인
+class A { // 자동으로 Object 클래스 상속받음
+	//오버라이드 메뉴에서 상속받은 메소드 보기
 }
