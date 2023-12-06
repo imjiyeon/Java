e2a73290accaf2553e53ab44bca872aef3316534
yoyt777 <yoyt777@gmail.com>
2023-12-06 17:03:39 +0900

    ㅁㅁㅁ

--- Project11/src/string/quiz/Quiz5.java
+++ Project11/src/string/quiz/Quiz5.java
@@ -3,22 +3,28 @@ package string.quiz;
 import java.util.*;
 
 /*
-* Scanner의 nextLine 메소드를 사용하여 문자열을 계속 입력받다가
-* 만약 "END"라는 문장이 입력되면 루프에서 빠져나와 프로그램을 종료하도록 구현하세요.
-* 
-* 단, end의 대소문자에 상관없이 종료해야 합니다 
+* Scanner 클래스의 nextLine 메소드를 사용하여 문자열을 입력받고,
+* "END" 또는 "end" 가 입력되면 루프를 종료하세요.
+*
+* while(true)를 사용하여 무한루프를 만드세요.
 * */
 
 public class Quiz5 {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
+		
 		while (true) {
+			
 			System.out.print("문장을 입력하세요:");
-			String line = sc.nextLine();
-			if (line.toUpperCase().equals("END")) { // 입력문장이 "END" 이면 종료
+			String line = sc.nextLine(); // 문자열 입력받기
+			
+			if (line.toUpperCase().equals("END")) { // 문자열이 "END" 또는 "end" 라면 루프 종료
 				break;
 			}
+			
 		}
+		
 	}
 }
