52566f2a0947233405d273441e603ebc61e29c1a
imjiyeon <yoyt12@naver.com>
2023-12-06 13:31:40 +0900

    ㅁㅁ

--- Project11/src/string/quiz/Quiz3.java
+++ Project11/src/string/quiz/Quiz3.java
@@ -3,23 +3,23 @@ package string.quiz;
 /*
  * 1. "자바 프로그래밍 입문"이라는 문자열을 만드세요.
  * 2. 공백을 제외한 글자수만 세어서 출력하세요.
- * 3. 이내용을 함수로 만들어서 호출해주세요.
  * 
  * ex) "자바 프로그래밍 입문" -> 9 (전체11 공백2)
  * */
 public class Quiz3 {
-	static int getCharCount(String s) {
-        int realLength = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != ' ') {  // 공백이 아닌 경우에만 결과값을 증가
+
+    public static void main(String[] args) {
+    	
+    	String str = "자바 프로그래밍 입문";
+    	
+    	int realLength = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {  // 공백이 아니면 숫자 증가
             	realLength++;
             }
         }
-        return realLength;
-    }
 
-    public static void main(String[] args) {
-    	System.out.println("자바 프로그래밍 입문".length());  // 11 출력
-        System.out.println(getCharCount("자바 프로그래밍 입문"));  // 9 출력
+    	System.out.println(str.length());  // 11
+        System.out.println(realLength);  // 9
     }
 }
