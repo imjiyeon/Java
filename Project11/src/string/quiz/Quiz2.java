f8885b3efa4a45791cc4ab2e60495807f4e7f9b3
imjiyeon <yoyt12@naver.com>
2023-12-06 13:25:38 +0900

    ㅁㅁ

--- Project11/src/string/quiz/Quiz2.java
+++ Project11/src/string/quiz/Quiz2.java
@@ -8,8 +8,11 @@ package string.quiz;
 public class Quiz2 {
 
 	public static void main(String[] args) {
+		
 		String str = "안녕하세요 저는 둘리입니다"; 
+		
 		System.out.println(str.indexOf("둘")); //"둘리"의 인덱스를 찾기
+		
 		System.out.println(str.substring(9,11)); //시작위치, 마지막위치(포함x)
 	}
 }
