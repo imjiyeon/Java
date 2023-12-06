2c0298365cb7fe3b55c25fcf25824b988dbb015d
imjiyeon <yoyt12@naver.com>
2023-12-06 10:34:05 +0900

    ㅁㅁ

--- Project11/src/object/Quiz3.java
+++ Project11/src/object/Quiz3.java
@@ -1,5 +1,18 @@
 package object;
 
+/*
+ * 1. name과 type을 멤버변수로 가지는 Dog클래스를 만드세요.
+ * 2. 다음과 같이 출력되도록 toString 메소드를 재정의하세요.
+ * System.out.println(dog) -> "진돗개 멍멍이"
+ * */
+public class Quiz3 {
+
+	public static void main(String[] args) {
+		Dog dog = new Dog("멍멍이", "진돗개");
+		System.out.println(dog);
+	}
+}
+
 class Dog{
 	String name;
 	String type;
@@ -11,19 +24,6 @@ class Dog{
 	
 	public String toString() {
 		return type + " " + name;
-	}
-}
-
-/*
- * 먼저 name과 type을 멤버변수로 가지는 Dog클래스를 만든다.
- * 그리고 다음 코드의 출력결과가 "진돗개 멍멍이"가 되도록 Dog클래스를 수정하세요.
- * 
- * */
-public class Quiz3 {
-
-	public static void main(String[] args) {
-		Dog dog = new Dog("멍멍이", "진돗개");
-		System.out.println(dog);
 	}
 }
 
