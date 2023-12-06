2d569f92859632c4c36dca786b3fd8f094782c4d
imjiyeon <yoyt12@naver.com>
2023-12-06 09:58:58 +0900

    ㅁㅁ

--- Project11/src/object/Ex3.java
+++ Project11/src/object/Ex3.java
@@ -36,20 +36,21 @@ public class Ex3 {
 		Student student1 = new Student(1001, "둘리");
 		Student student2 = new Student(1001, "둘리");
 		
-		//두 참조 변수의 값(메모리주소)을 비교한다
-		if (student1 == student2) { //100번지 == 200번지
+		//비교연산자로 비교
+		if (student1 == student2) { //메모리 주소로 비교. (100번지 == 200번지)
 			System.out.println("student1와 student2의 주소는 같습니다.");
 		} else {
 			System.out.println("student1와 student2의 주소는 다릅니다.");
 		}
 		
-		//equals 원형 메소드로 두 학생의 메모리 주소를 비교한다 
-		//재정의된 equals 메소드로 두 학생의 학번을 비교한다 
-		if (student1.equals(student2)) { //100번지 == 200번지 또는 1001 == 1001
+		//equals 메소드로 비교
+		if (student1.equals(student2)) { //학생의 학번으로 비교. (1001 == 1001)
 			System.out.println("student1와 student2는 같은 학생입니다.");
 		} else {
 			System.out.println("student1와 student2는 다른 학생입니다.");
 		}
+		
+		//Q. 재정의함수를 주석처리해서 원형메소드로 비교하기 
 
 	}
 }
