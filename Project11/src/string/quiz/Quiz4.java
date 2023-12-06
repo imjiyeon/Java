d683c2060aaaa1efb4315e91de013d9dc823ce52
yoyt777 <yoyt777@gmail.com>
2023-12-06 18:32:07 +0900

    ㅁㅁㅁ

--- Project11/src/string/quiz/Quiz4.java
+++ Project11/src/string/quiz/Quiz4.java
@@ -4,6 +4,7 @@ package string.quiz;
  * 이전 문제를 함수형태로 바꾸고 호출하세요.
  * */
 public class Quiz4 {
+	
 	static int getCharCount(String s) {
         int realLength = 0;
         for (int i = 0; i < s.length(); i++) {
@@ -15,7 +16,12 @@ public class Quiz4 {
     }
 
     public static void main(String[] args) {
-    	System.out.println("자바 프로그래밍 입문".length());  // 11 출력
-        System.out.println(getCharCount("자바 프로그래밍 입문"));  // 9 출력
+    	
+    	System.out.println("자바 프로그래밍 입문".length());  // 공백을 포함한 길이는 11
+    	
+        System.out.println(getCharCount("자바 프로그래밍 입문"));  // 공백을 제거한 길이는 9
+        
+        System.out.println(getCharCount("equals 메소드는 두 객체가 같은지 비교한다")); // 21
+        
     }
 }
