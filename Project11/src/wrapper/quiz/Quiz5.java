945421a2d4571a0538b16d3d425f30c03d277937
imjiyeon <yoyt12@naver.com>
2023-12-07 13:15:00 +0900

    ㅁㅁㅁ

--- Project11/src/wrapper/quiz/Quiz5.java
+++ Project11/src/wrapper/quiz/Quiz5.java
@@ -14,11 +14,12 @@ public class Quiz5 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		String num1 = sc.nextLine(); //문자열 입력받기
+		String num1 = sc.nextLine(); //첫번째 숫자 입력
 		
-		String num2 = sc.nextLine();
+		String num2 = sc.nextLine(); //두번째 숫자 입력
 		
-		int sum = Integer.parseInt(num1) + Integer.parseInt(num2);
+		//문자열을 숫자로 변환하고, 덧셈 수행
+		int sum = Integer.parseInt(num1) + Integer.parseInt(num2); 
 		
 		System.out.println("두 수의 합: " + sum);
 		
