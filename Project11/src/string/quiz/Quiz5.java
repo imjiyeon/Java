97a4aebb4b5691c5240a67784acc609e592a41f5
imjiyeon <yoyt12@naver.com>
2023-12-07 10:35:55 +0900

    ㅁㅁㅁ

--- Project11/src/string/quiz/Quiz5.java
+++ Project11/src/string/quiz/Quiz5.java
@@ -14,24 +14,31 @@ public class Quiz5 {
 
 		// 문자열 입력받기
 		Scanner scanner = new Scanner(System.in);
-		String str = scanner.nextLine(); 
+		String str = scanner.nextLine(); // 안녕하세요
+		
+		// 1. 하드코딩
+		System.out.print(str.charAt(4));
+		System.out.print(str.charAt(3));
+		System.out.print(str.charAt(2));
+		System.out.print(str.charAt(1));
+		System.out.print(str.charAt(0));
 
-		// 1-1.문자열에서 뒤에서부터 한 글자씩 출력
+		// 2.문자열에서 뒤에서부터 한 글자씩 출력
 		int i = str.length() - 1;
 		while(i>=0) {
 			System.out.print(str.charAt(i));
 			i--;
 		}
 		
-//		// 1-2.
-//		for (int j = str.length() - 1; j >= 0; j--) { // 1씩 감소
-//			System.out.print(str.charAt(j));
-//		}
-//
-//		// 2.StringBuilder 클래스 사용
-//		StringBuilder builder = new StringBuilder(str);
-//		builder.reverse(); //문자열을 거꾸로 변경하기
-//		System.out.println(builder.toString());
+		// 3.
+		for (int j = 4; j >= 0; j--) { // 1씩 감소
+			System.out.print(str.charAt(j));
+		}
+
+		// 4.StringBuilder 클래스 사용
+		StringBuilder builder = new StringBuilder(str);
+		builder.reverse(); //문자열을 거꾸로 변경하기
+		System.out.println(builder.toString());
 
 	}
 
