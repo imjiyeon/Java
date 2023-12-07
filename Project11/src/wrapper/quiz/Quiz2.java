879377ad75b145ede525534cc72be367ba9824cc
imjiyeon <yoyt12@naver.com>
2023-12-07 12:17:08 +0900

    ㅁㅁㅁ

--- Project11/src/wrapper/quiz/Quiz2.java
+++ Project11/src/wrapper/quiz/Quiz2.java
@@ -3,21 +3,22 @@ package wrapper.quiz;
 /*
  * <wrapper 클래스 메소드 사용하기>
  * 
- * 문자열 "10"을 Byte, Short, Integer, Long타입 변수에 저장하세요.
- * 문자열 "10.0" 을 Float, Double타입 변수에 저장하세요.
+ * 문자열 "10"을 byte, short, int, long 으로 변환하세요.
+ * 문자열 "10.0" 을 float, double 로 변환하세요.
  * */
+
 public class Quiz2 {
 
 	public static void main(String[] args) {
 
-		// 문자열을 숫자타입 변수에 저장하려면 "parse" 메소드를 사용해야함	
-		Byte bNum = Byte.parseByte("10"); //"10" -> 10
-		Short sNum = Short.parseShort("10");
-		Integer iNum = Integer.parseInt("10");
-		Long lNum = Long.parseLong("10");
+		// 문자열을 숫자로 변환하려면 "parse" 메소드를 사용해야함	
+		byte bNum = Byte.parseByte("10"); //"10" -> 10
+		short sNum = Short.parseShort("10");
+		int iNum = Integer.parseInt("10");
+		long lNum = Long.parseLong("10");
 		
-		Float fNum = Float.parseFloat("10.0"); //"10.0" -> 10.0
-		Double dNum = Double.parseDouble("10.0");
+		float fNum = Float.parseFloat("10.0"); //"10.0" -> 10.0
+		double dNum = Double.parseDouble("10.0");
 		
 	}
 }
