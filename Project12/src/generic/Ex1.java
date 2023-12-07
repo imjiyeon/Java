45303628c823046fa2ed00634b4ba2d175415aa0
yoyt777 <yoyt777@gmail.com>
2023-12-07 20:32:36 +0900

    ㅁㅁㅁ

--- Project12/src/generic/Ex1.java
+++ Project12/src/generic/Ex1.java
@@ -1,41 +1,40 @@
 package generic;
 
 /*
- * 파우더와 플라스틱 액체를 사용해서 입체모형을 만드는 3D 프린터기 구현하기
+ * 여러가지 재료를 사용하는 3D 프린터 클래스 만들기
  * */
 
-//첫번째 재료인 Powder 클래스 정의하기
+//재료로 사용할 파우더 클래스 만들기
 class Powder {
 
 	public void doPrinting() {
 		System.out.println("Powder 재료로 출력합니다");
 	}
-	
+
 	public String toString() {
 		return "자료는 Powder 입니다";
 	}
-	
+
 }
 
-//두번째 재료인 Plastic 클래스 정의하기
+//재료로 사용할 플라스틱 클래스 만들기
 class Plastic {
 
 	public void doPrinting() {
 		System.out.println("Plastic 재료로 출력합니다");
 	}
-	
+
 	public String toString() {
 		return "자료는 Plastic 입니다";
 	}
-	
+
 }
 
-//파우더를 재료로 모형을 출력하는 프린터 정의하기
+//파우더를 재료로 사용하는 프린터 만들기
 class ThreeDPrinter1 {
-	
-	//재료가 파우더인 경우
-	private Powder material; //재료 변수의 자료형을 Powder로 선언
-	
+
+	private Powder material; // 변수의 자료형을 Powder로 선언
+
 	public void setMaterial(Powder powder) {
 		this.material = powder;
 	}
@@ -46,11 +45,10 @@ class ThreeDPrinter1 {
 
 }
 
-//파우더를 재료로 모형을 출력하는 프린터 정의하기
+//플라스틱을 재료로 사용하는 프린터 만들기
 class ThreeDPrinter2 {
-	
-	//재료가 플라스틱인 경우
-	private Plastic material; //재료 변수의 자료형을 Plastic로 선언
+
+	private Plastic material; // 변수의 자료형을 Plastic으로 선언
 
 	public Plastic getMaterial() {
 		return material;
