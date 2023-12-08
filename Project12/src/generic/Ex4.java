0e6f966e093db23597c6628fef9be198690c4380
imjiyeon <yoyt12@naver.com>
2023-12-08 09:50:46 +0900

    ㅁㅁㅁㅁ

--- Project12/src/generic/Ex4.java
+++ Project12/src/generic/Ex4.java
@@ -10,11 +10,13 @@ public class Ex4 {
 
 	public static void main(String[] args) {
 		
+		//숫자를 저장하는 리스트 생성
 		ArrayList<Integer> list1 = new ArrayList<>(); //리스트의 자료형을 int로 지정
 		list1.add(10);	//int타입의 값만 저장 가능
 		Integer i = list1.get(0); //값을 꺼낼 때 형변환 할 필요 없음
 		System.out.println(i); 
 		
+		//불리언을 사용하는 리스트 생성
 		ArrayList<Boolean> list2 = new ArrayList<>();
 		list2.add(true);
 		Boolean bool = list2.get(0);
