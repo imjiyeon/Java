3773969613d3bf785f924518bf7efcd4fe6cc46d
imjiyeon <yoyt12@naver.com>
2023-12-08 09:50:14 +0900

    ㅁㅁㅁ

--- Project12/src/generic/Ex4.java
+++ Project12/src/generic/Ex4.java
@@ -3,15 +3,16 @@ package generic;
 import java.util.ArrayList;
 
 /*
- * ArrayList 클래스에서 사용하는 제네릭
- * ArrayList 클래스 내부 구조 보기 (타입변수,get메소드)
+ * 제네릭을 사용하는 ArrayList 클래스 사용하기
+ * ArrayList 클래스의 내부 구조 보기 (타입변수, get메소드 등)
  * */
 public class Ex4 {
 
 	public static void main(String[] args) {
+		
 		ArrayList<Integer> list1 = new ArrayList<>(); //리스트의 자료형을 int로 지정
 		list1.add(10);	//int타입의 값만 저장 가능
-		Integer i = list1.get(0); //값 꺼낼 때 형변환 생략 가능
+		Integer i = list1.get(0); //값을 꺼낼 때 형변환 할 필요 없음
 		System.out.println(i); 
 		
 		ArrayList<Boolean> list2 = new ArrayList<>();
