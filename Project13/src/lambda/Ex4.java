6f1f4b19a372d2f4e0021b6173d5c91e634d596b
yoyt777 <yoyt777@gmail.com>
2023-12-11 20:31:01 +0900

    ㅇㅇㅇ

--- Project13/src/lambda/Ex4.java
+++ Project13/src/lambda/Ex4.java
@@ -24,22 +24,19 @@ public class Ex4 {
 		// Collections.sort(): 리스트의 데이터를 순서대로 정렬하는 기능
 		// 인자: 문자열 리스트, 리스트 정렬하는 로직을 가지고 있는 Comparator 클래스
 
-		// 1.클래스에 메소드를 정의하고 사용
-		Collections.sort(list, new ComparatorImpl());
+		// 익명 클래스 사용
+		Collections.sort(list, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.compareTo(o2); // 알파벳 순정렬. (비교해서 작은 알파벳이 앞으로 옴)
+			}
+		});
 		System.out.println("리스트 역정렬: " + list);
 
-		// 2.람다식 함수 사용
+		// 익명클래스을 람다식으로 변경 가능
 		Collections.sort(list, (s1, s2) -> s1.compareTo(s2));
 		System.out.println("리스트 순정렬: " + list);
-	}
-
-}
-
-class ComparatorImpl implements Comparator<String> {
-
-	@Override
-	public int compare(String o1, String o2) {
-		return o1.compareTo(o2); // 알파벳 순정렬. (비교해서 작은 알파벳이 앞으로 옴)
+		
 	}
 
 }
