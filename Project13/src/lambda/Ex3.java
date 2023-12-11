ce14ed311f84cd839049de75379bbc4ca04585ad
yoyt777 <yoyt777@gmail.com>
2023-12-11 20:26:04 +0900

    ddd

--- Project13/src/lambda/Ex3.java
+++ Project13/src/lambda/Ex3.java
@@ -1,32 +1,37 @@
 package lambda;
 
 /*
- * 람다 활용하기1
- * - 두 문자를 연결하는 함수 만들기
+ * 람다식 활용하기 (1)
  * */
 
-//함수형 인터페이스 선언하기
+//함수형 인터페이스 선언
 interface StringConcat {
-	public void makeString(String s1, String s2);
+	
+	// 두 문자열을 연결하는 메소드 선언
+	public void makeString(String s1, String s2); 
+}
+
+//구현 클래스 만들기
+class StringConcatImpl implements StringConcat {
+
+	@Override
+	public void makeString(String s1, String s2) {
+		System.out.println(s1 + "," + s2);
+	}
+
 }
 
 public class Ex3 {
-	public static void main(String[] args) {
-		String s1 = "Hello";
-		String s2 = "World";
-		
-		//람다식 함수 사용하기
-		StringConcat concat1 = (str1, str2)-> System.out.println(str1 + "," + str2 );
 
-		//익명 클래스 사용하기
-		StringConcat concat2 = new StringConcat() {
-			@Override
-			public void makeString(String str1, String str2) {
-				System.out.println( str1 + "," + str2 );
-			}
-		};
-		
-		concat1.makeString(s1, s2);
-		concat2.makeString(s1, s2);
+	public static void main(String[] args) {
+
+		// 구현 클래스 사용하기
+		StringConcat concat1 = new StringConcatImpl();
+		concat1.makeString("Hello", "World");
+
+		// 람다식 함수 사용하기
+		StringConcat concat2 = (str1, str2) -> System.out.println(str1 + "," + str2);
+		concat2.makeString("Hello", "World");
 	}
+
 }
