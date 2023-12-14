3629eac45a710afcefed511ad17e1131262096da
yoyt777 <yoyt777@gmail.com>
2023-12-14 17:55:46 +0900

    dddd

--- Project13/src/stream/quiz/Quiz3.java
+++ Project13/src/stream/quiz/Quiz3.java
@@ -1,65 +1,59 @@
 package stream.quiz;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Quiz3 {
 
 	public static void main(String[] args) {
-		
-		// 거래내역 리스트 생성
-		List<Order> list = new ArrayList<>();
-		
-		list.add(new Order(1001, 2022, 300));
-		list.add(new Order(1002, 2022, 150));
-		list.add(new Order(1003, 2022, 200));
-		list.add(new Order(1004, 2023, 500));
-		list.add(new Order(1005, 2023, 400));
-		list.add(new Order(1006, 2023, 600));
-		list.add(new Order(1007, 2023, 650));
-		
-		// 모든 거래번호를 한줄에 나열하세요.
-		// -> 1001 1002 1003 1004 1005 1006 1007
-		Stream<Order> stream1 = list.stream();
-		stream1.map(o -> o.orderNo).forEach(s-> System.out.print(s + " "));
-		
-		// 2022년도의 거래 총금액과 건수를 출력하세요
-		// -> 총금액: 650, 거래건수: 3
-		Stream<Order> stream2 = list.stream();
-		int sum1 = stream2.filter(o -> o.year == 2022).mapToInt(o -> o.price).sum();
-		System.out.println("2022년도의 거래 총금액: " + sum1);
-				
-		Stream<Order> stream3 = list.stream();
-		long count1 = stream3.filter(o -> o.year == 2022).count();
-		System.out.println("2022년도의 거래건수: " + count1);
-		
-		// 2023년도의 거래 총금액과 건수를 출력하세요
-		// -> 총금액: 2150, 거래건수: 4
-		Stream<Order> stream4 = list.stream();
-		int sum2 = stream4.filter(o -> o.year == 2023).mapToInt(o -> o.price).sum();
-		System.out.println("2023년도의 거래 총금액: " + sum2);
-		
-		Stream<Order> stream5 = list.stream();
-		long count2 = stream5.filter(o -> o.year == 2023).count();
-		System.out.println("2023년도의 거래건수: " + count2);		
-						
-	}
 
-}
+		// 파일 목록 생성
+		String[] arr = { "file1.txt", "file2.pdf", "file3.txt", "file4.img", "file5.txt", "file6.img" };
 
-// 쇼핑몰 거래내역 클래스
-class Order {
+		// 1.파일 목록 중에서 txt 파일만 찾으세요
+		// 결과: file1.txt file3.txt file5.txt
+		Stream<String> stream = Arrays.stream(arr);		
+		stream
+				.filter(s -> s.endsWith(".txt")) //문자열이 .txt로 끝나는지 확인
+				.forEach(s -> System.out.print(s + " "));
 
-	int orderNo; // 주문번호
-	int year; // 주문연도
-	int price; // 거래금액 (만원)
+		// 2.확장자별로 파일의 개수를 구하세요
+		// txt파일의 개수: 3
+		// pdf파일의 개수: 1
+		// img파일의 개수: 2
+		Stream<String> stream2 = Arrays.stream(arr);
+		long txtCount = stream2
+								.filter(s -> s.endsWith(".txt"))
+								.count();
 
-	public Order(int orderNo, int year, int price) {
-		super();
-		this.orderNo = orderNo;
-		this.year = year;
-		this.price = price;
+		Stream<String> stream3 = Arrays.stream(arr);
+		long pdfCount = stream3
+								.filter(s -> s.endsWith(".pdf"))
+								.count();
+
+		Stream<String> stream4 = Arrays.stream(arr);
+		long imgCount = stream4
+								.filter(s -> s.endsWith(".img"))
+								.count();
+
+		System.out.println("txt파일의 개수: " + txtCount);
+		System.out.println("pdf파일의 개수: " + pdfCount);
+		System.out.println("img파일의 개수: " + imgCount);
+
+		// 3. 파일의 확장자만 추출하여 출력하세요 (hint! (.)점은 문자기호로 (\\.)으로 표현합니다 )
+		// 결과: txt pdf img
+		Stream<String> stream5 = Arrays.stream(arr);
+		stream5
+				.map(s -> {
+					// .을 구분자를 사용하여 문자열을 자름. (점은 특수기호로 표현해야함)
+					String[] strArr = s.split("\\."); 
+					return strArr[1];
+				})
+				.distinct()
+				.forEach(s -> System.out.print(s + " "));
+
 	}
 
 }
