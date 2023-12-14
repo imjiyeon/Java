69c23f787d9097bd70da684cf420ae81099f339b
imjiyeon <yoyt12@naver.com>
2023-12-14 11:25:40 +0900

    ㅇㅇㅇ

--- Project13/src/stream/quiz/Quiz2.java
+++ Project13/src/stream/quiz/Quiz2.java
@@ -13,10 +13,14 @@ public class Quiz2 {
 		String[] arr = { "file1.txt", "file2.pdf", "file3.txt", "file4.img", "file5.txt", "file6.img" };
 
 		// 1.파일 목록 중에서 txt 파일만 찾으세요
+		// 결과: file1.txt file3.txt file5.txt
 		Stream<String> stream = Arrays.stream(arr);
-		stream.filter(s -> s.endsWith(".txt")).forEach(s -> System.out.println(s));
+		stream.filter(s -> s.endsWith(".txt")).forEach(s -> System.out.print(s + " "));
 
 		// 2.확장자별로 파일의 개수를 구하세요
+		// txt파일의 개수: 3
+		// pdf파일의 개수: 1
+		// img파일의 개수: 2
 		Stream<String> stream2 = Arrays.stream(arr);
 		long txtCount = stream2.filter(s -> s.endsWith(".txt")).count();
 
@@ -31,11 +35,12 @@ public class Quiz2 {
 		System.out.println("img파일의 개수: " + imgCount);
 
 		// 3. 파일의 확장자만 추출하여 출력하세요 (hint! (.)점은 문자기호로 (\\.)으로 표현합니다 )
+		// 결과: txt pdf img
 		Stream<String> stream5 = Arrays.stream(arr);
 		stream5.map(s -> {
 			String[] strArr = s.split("\\.");
 			return strArr[1];
-		}).distinct().forEach(s -> System.out.print(s + ", "));
+		}).distinct().forEach(s -> System.out.print(s + " "));
 
 	}
 
