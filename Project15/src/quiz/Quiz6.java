aaed9f70e84bd4d1fefaa86e3b8ffe99d50301be
imjiyeon <yoyt12@naver.com>
2023-12-19 10:32:24 +0900

    ㅇㅇㅇ

--- Project15/src/quiz/Quiz6.java
+++ Project15/src/quiz/Quiz6.java
@@ -12,18 +12,21 @@ public class Quiz6 {
 
 	public static void main(String[] args) {
 		try {
-			
+
 			// 출력 스트림 생성
 			FileWriter fw = new FileWriter("quiz6.txt");
 
 			// Writer는 문자열 출력 가능
-			for (int i = 2; i <= 9; i++) {
-				for (int j = 1; j <= 9; j++) {
+			for (int i = 2; i <= 9; i++) { // 단
+
+				for (int j = 1; j <= 9; j++) { // 곱하는 수
+
 					fw.write(i + "*" + j + "=" + i * j + "\n"); // 줄바꿈은 특수기호로 처리
 				}
+
 				fw.write("\n");
 			}
-			
+
 			fw.flush();
 
 		} catch (IOException e) {
