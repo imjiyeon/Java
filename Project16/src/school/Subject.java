620d8f6f5ceeaf5af8e79003bbff02156d16c61a
imjiyeon <yoyt12@naver.com>
2023-12-20 13:32:11 +0900

    ddd

--- Project16/src/school/Subject.java
+++ Project16/src/school/Subject.java
@@ -10,15 +10,15 @@ public class Subject {
 	
 	String subjectName; // 과목 이름
 	int subjectId; // 과목 고유 번호
-	int gradeType; // 과목 종류 (1: 필수, 2: 교양)
+	int subjectType; // 과목 구분 (1: 필수, 2: 교양)
 
 	// 이 과목을 수강하는 학생 리스트
 	public ArrayList<Student> studentList;
 
-	public Subject(String subjectName, int subjectId, int gradeType) {
+	public Subject(String subjectName, int subjectId, int subjectType) {
 		this.subjectName = subjectName;
 		this.subjectId = subjectId;
-		this.gradeType = gradeType;
+		this.subjectType = subjectType;
 		studentList = new ArrayList<Student>(); // 리스트 생성
 	}
 
