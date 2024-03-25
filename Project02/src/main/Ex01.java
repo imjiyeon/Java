561fc95e379757d294bf7c5c8f81901920278564
geoblo <geoblo@naver.com>
2024-03-25 12:13:09 +0900

    sdsd

--- Project02/src/main/Ex01.java
+++ Project02/src/main/Ex01.java
@@ -3,6 +3,8 @@ package main;
 /*
  * 변수 선언 및 사용
  * */
+//주석 단축키 - 블럭 선택 > 컨트롤 + /
+//주석 단축키 - 블럭 선택 > 컨트롤 + 시프트 + /
 public class Ex01 {
 	public static void main(String[] args) {
 		int x; // 정수형 변수 선언
