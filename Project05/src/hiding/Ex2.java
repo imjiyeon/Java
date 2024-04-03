2b983bcb97a506b237c2fb83080897b44d28c14f
geoblo <geoblo@naver.com>
2024-04-04 08:55:04 +0900

    ddㅇㅇ

--- Project05/src/hiding/Ex2.java
+++ Project05/src/hiding/Ex2.java
@@ -1,42 +1,54 @@
 package hiding;
 
 /*
- * private 변수 선언하고 값 변경하기
+ * private 변수과 public 메소드
  * */
 public class Ex2 {
 	public static void main(String[] args) {
-		MyDate2 date = new MyDate2(); // 달력 생성
-		// date.day = 30; //private 변수는 외부에서 직접 사용할 수 없음
+
+		MyDate2 date = new MyDate2(); // 날짜 생성
+
 		date.setMonth(2);
-		date.setDay(30); // 검증 실패하여 저장되지 않음
-		date.setDay(10); // 10일 저장 성공
+
+		//private 변수는 외부에서 직접 사용할 수 없음
+		// date.day = 30;
+
+		date.setDay(30); // 검증 실패하여 저장 실패
 		System.out.println("현재 날짜는 " + date.getMonth() + "월 " + date.getDay() + "일 입니다");
 
-		// private변수는 잘못된 값이 저장되는 것을 막을 수 있음
+		date.setDay(10); // 저장 성공
+		System.out.println("현재 날짜는 " + date.getMonth() + "월 " + date.getDay() + "일 입니다");
 	}
 }
 
+// 이름이 중복되니까 "2"를 붙여주세요!
 class MyDate2 {
+	// private 멤버변수
 	private int month; // 월
 	private int day; // 일
 
+	// public 메소드
+	// 월을 가져오는 메소드
 	public int getMonth() {
 		return month;
 	}
 
+	// 월을 변경하는 메소드
 	public void setMonth(int month) {
 		this.month = month;
 	}
 
+	// 일을 가져오는 메소드
 	public int getDay() {
 		return day;
 	}
 
-	// 새로들어온 값이 올바른 일자인지 확인하고 저장한다
+	// 일을 변경하는 메소드
+	// 새로들어온 값이 올바른 일자인지 확인하고 저장
 	public void setDay(int day) {
-		// month가 2월 일때, 새로운일자가 1~28일이라면 저장
+		// month가 2월 인 경우, 1~28일에 포함되면 저장
 		if (month == 2) { // 1,3,4,...
-			if (day < 1 || day > 28) { // 아니라면 값을 저장하지 않는다
+			if (day < 1 || day > 28) { // 아니라면 값을 저장안함
 				System.out.println("2월에는 " + day + " 일이 없습니다!!");
 				return;
 			}
--- Project05/src/hiding/Ex3.java
+++ Project05/src/hiding/Ex3.java
@@ -4,6 +4,17 @@ package hiding;
  * getter setter 메소드 generate 기능 사용하기
  * */
 public class Ex3 {
+
+	public static void main(String[] args) {
+
+		Person person = new Person();
+
+		// 참조변수로 어떤 메소드가 있는지 확인하기
+//		person.setName("둘리");
+//		String name = person.getName();
+
+	}
+
 }
 
 class Person {
