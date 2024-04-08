c097016f0d0cf84084261d69ffc18e415ed2ce86
geoblo <geoblo@naver.com>
2024-04-08 10:47:25 +0900

    ddd

--- Project07/src/array/quiz/Quiz3.java
+++ Project07/src/array/quiz/Quiz3.java
@@ -3,14 +3,17 @@ package array.quiz;
 
 //  5개 크기인 정수형 배열을 생성하고, 다음과 같이 초기화하세요
 //  { 10, 15, 20, 25, 30 }
+//
 //  반복문을 사용하여 배열요소의 합을 구하고 출력하세요.
+//  합계: 100
 
 public class Quiz3 {
 
 	public static void main(String[] args) {
 
 		int[] arr = new int[] { 10, 15, 20, 25, 30 };
-		int sum = 0; //합을 저장할 변수
+		
+		int sum = 0; //합을 저장할 변수 선언
 		
 		for(int i=0; i<5; i++){ 
 			sum = sum + arr[i];
