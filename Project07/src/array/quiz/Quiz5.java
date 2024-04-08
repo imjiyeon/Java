f7ce717cbc7c52b546799b26e87fe3ae14c009c8
geoblo <geoblo@naver.com>
2024-04-08 11:11:55 +0900

    ddd

--- Project07/src/array/quiz/Quiz5.java
+++ Project07/src/array/quiz/Quiz5.java
@@ -1,5 +1,4 @@
-package array.quiz;
-
+package array;
 
 //  5개 크기의 정수형 배열을 생성하고, 다음과 같이 초기화하세요
 //  {5, 20, 100, 30, 77}
@@ -10,15 +9,24 @@ package array.quiz;
 public class Quiz5 {
 
 	public static void main(String[] args) {
-		int[] arr = new int[] {5, 20, 100, 30, 77};	
-		int max; //최대값을 저장하기 위한 변수
-		max = arr[0]; //배열의 첫번째 값으로 최대값을 초기화	
-		for(int i=1; i<5; i++) {
-			if(arr[i] > max) { //배열의값이 현재 최대값보다 크다면
-				max = arr[i]; //최대값을 변경
+
+		int[] arr = new int[] { 5, 20, 100, 30, 77 };
+
+		// 최대값을 저장하기 위한 변수
+		// 배열의 첫번째 값으로 최대값을 초기화
+		int max = arr[0];
+
+		for (int i = 1; i < 5; i++) {
+
+			System.out.print(arr[i] + " vs "  + max);
+
+			if (arr[i] > max) { // 배열의값이 현재 최대값보다 크면
+				max = arr[i]; // 최대값 변경
 			}
+			System.out.print("?? " + max);
+			System.out.println();
 		}
-		
+
 		System.out.println(max);
 	}
 }
