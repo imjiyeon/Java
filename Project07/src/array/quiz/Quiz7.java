8974a27a7c8aa2e9ae51ed4823e7cf590e602d7c
geoblo <geoblo@naver.com>
2024-04-08 10:05:40 +0900

    ddd

--- Project07/src/array/quiz/Quiz7.java
+++ Project07/src/array/quiz/Quiz7.java
@@ -1,10 +1,11 @@
 package array.quiz;
 
-/*
- * System.arraycopy 메소드를 사용하여 배열을 복사하세요.
- * 원본배열: {'a', 'b', 'c', 'd', 'e'}
- * 새배열: {'d', 'e'}
- * */
+
+//  System.arraycopy 메소드를 사용하여 배열을 복사하세요.
+//
+//  원본배열: {'a', 'b', 'c', 'd', 'e'}
+//  새배열: {'d', 'e'}
+
 public class Quiz7 {
 
 	public static void main(String[] args) {
