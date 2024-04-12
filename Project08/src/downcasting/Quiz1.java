bf1e9d0a2a4b2b69f26c9e39a19b05779e293a7f
geoblo <geoblo@naver.com>
2024-04-12 12:36:55 +0900

    ㅇㄹㅇㄹ

--- Project08/src/downcasting/Quiz1.java
+++ Project08/src/downcasting/Quiz1.java
@@ -13,15 +13,18 @@ import java.util.ArrayList;
 public class Quiz1 {
 
 	public static void main(String[] args) {
-		// 사람,호랑이,독수리를 담을 배열 생성
-		ArrayList<Animal> aniList = new ArrayList<Animal>(); // 베열의 자료형은 Animal로 지정
+
+		// Animal 타입의 리스트를 생성하면, 사람 호랑이 독수리 저장 가능
+		ArrayList<Animal> aniList = new ArrayList<Animal>();
+
 		aniList.add(new Human()); // 리스트에 사람 추가
 		aniList.add(new Tiger()); // 리스트에 호랑이 추가
 		aniList.add(new Eagle()); // 리스트에 독수리 추가
 
+		// for문으로 리스트의 요소를 하나씩 꺼냄
 		for (int i = 0; i < aniList.size(); i++) {
 
-			Animal ani = aniList.get(i); // 배열의 요소를 꺼내서 Animal형 변수에 저장
+			Animal ani = aniList.get(i); //요소를 꺼내서 Animal형 변수에 저장
 
 			if (ani instanceof Human) { // 인스턴스의 타입이 Human형이면
 				Human h = (Human) ani; // Human형으로 다운캐스팅
