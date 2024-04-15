73c8e3c3c134d4b49f2fd5a7129b8ba86a64e2b6
geoblo <geoblo@naver.com>
2024-04-15 12:46:20 +0900

    ㅀㅀ

--- Project09/src/abstractex/quiz/Quiz2.java
+++ Project09/src/abstractex/quiz/Quiz2.java
@@ -25,8 +25,9 @@ public class Quiz2 {
 }
 
 //자동차 클래스
-abstract class Car2 { // 추상클래스로 지정
-	// 추상메소드 선언
+abstract class Car2 {
+	
+	// 자동차가 달리고, 기름을 넣는 기능은 모든 자동차가 가지고 있는 기능
 	public abstract void run();
 
 	public abstract void refuel();
@@ -46,14 +47,14 @@ class Bus extends Car2 { // 상속받기
 		System.out.println("천연 가스를 충전 합니다.");
 	}
 
-	// 버스에 승객을 태우는 메소드 추가
+	// 버스에 승객을 태우는 기능은 버스만 사용함
 	public void takePassenger() {
 		System.out.println("승객을 버스에 태웁니다.");
 	}
 }
 
 //오토카 클래스
-class AutoCar extends Car2 { // 상속받기
+class AutoCar extends Car2 {
 
 	@Override
 	public void run() { // 물려받은 추상메소드 구현
@@ -65,7 +66,7 @@ class AutoCar extends Car2 { // 상속받기
 		System.out.println("휘발유를 주유합니다.");
 	}
 
-	// 차에 짐을 싣는 메소드 추가
+	// 차에 짐을 싣는 기능은 오토카만 사용함
 	public void load() {
 		System.out.println("짐을 싣습니다.");
 	}
