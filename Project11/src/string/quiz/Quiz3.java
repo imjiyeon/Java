67cf6f51adc6b115672665a403d25493f640c694
geoblo <geoblo@naver.com>
2024-04-17 11:17:57 +0900

    ㅇㅇㅇ

--- Project11/src/string/quiz/Quiz3.java
+++ Project11/src/string/quiz/Quiz3.java
@@ -4,7 +4,7 @@ package string.quiz;
 //  "자바 프로그래밍 입문"이라는 문자열을 만드세요.
 //  공백을 제외한 글자수만 세어서 출력하세요.
 //
-//  ex) "자바 프로그래밍 입문" -> 9 (전체11 공백2)
+//  ex) "자바 프로그래밍 입문" -> 9 (전체:11 공백:2)
 
 public class Quiz3 {
 
@@ -14,6 +14,9 @@ public class Quiz3 {
     	
     	int realLength = 0;
         for (int i = 0; i < str.length(); i++) {
+            
+            //공백도 문자로 취급함
+            
             if (str.charAt(i) != ' ') {  // 공백이 아니면 숫자 증가
             	realLength++;
             }
