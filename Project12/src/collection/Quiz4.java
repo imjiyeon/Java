caed694228e61172a18af3a99333110b50d44796
geoblo <geoblo@naver.com>
2024-05-03 12:04:18 +0900

    DDDㅇㅇ

--- Project12/src/collection/Quiz4.java
+++ Project12/src/collection/Quiz4.java
@@ -1,7 +1,6 @@
 package collection;
 
 import java.util.ArrayList;
-//import collection.Member;
 
 /*
  * ArrayList를 활용해 쇼핑몰 회원관리 프로그램 구현하기
@@ -9,23 +8,25 @@ import java.util.ArrayList;
 public class Quiz4 {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("MemberArrayList를 생성하고 4명의 회원을 추가하세요!");
-		
+
 		MemberArrayList memberArrayList = new MemberArrayList();
+
+		// 회원 4명 추가
 		Member member1 = new Member(1001, "둘리");
 		Member member2 = new Member(1002, "또치");
 		Member member3 = new Member(1003, "도우너");
 		Member member4 = new Member(1004, "고길동");
-
-		// 회원 4명 추가
 		memberArrayList.addMember(member1);
 		memberArrayList.addMember(member2);
 		memberArrayList.addMember(member3);
 		memberArrayList.addMember(member4);
+
 		memberArrayList.showAllMember();
 
 		System.out.println("MemberArrayList에 1004번 회원이 있으면 삭제하세요!");
+
 		// 아이디로 4번째 회원 삭제
 		memberArrayList.removeMember(1004);
 		memberArrayList.showAllMember();
@@ -39,7 +40,7 @@ class MemberArrayList {
 	ArrayList<Member> list; // 리스트 선언
 
 	public MemberArrayList() {
-		list = new ArrayList<Member>(); // 리스트 생성
+		list = new ArrayList<Member>(); // 리스트를 먼저 생성해야함
 	}
 
 	// 리스트에 회원을 추가하는 메소드
@@ -47,27 +48,31 @@ class MemberArrayList {
 		list.add(member);
 	}
 
-	// Q.아이디로 회원을 삭제하는 메소드
-	public boolean removeMember(int memberId) {
+	// 아이디로 회원을 삭제하는 메소드
+	public void removeMember(int memberId) {
+
 		for (int i = 0; i < list.size(); i++) {
 			Member member = list.get(i);
 			int id = member.memberId;
+
 			if (id == memberId) {
 				list.remove(i);
 				System.out.println(memberId + "번 회원을 삭제하였습니다");
-				return true; // 삭제에 성공했으면 true 반환
+				return;
 			}
 		}
+
 		System.out.println(memberId + "번 회원이 존재하지 않습니다");
-		return false; // 삭제에 실패했으면 false 반환
 	}
 
-	// Q.전체 회원을 출력하는 메소드
+	// 전체 회원을 출력하는 메소드
 	public void showAllMember() {
+
 		for (int i = 0; i < list.size(); i++) {
 			Member member = list.get(i);
 			System.out.println(member);
 		}
+
 		System.out.println();
 	}
 }
--- Project12/src/collection/Quiz5.java
+++ Project12/src/collection/Quiz5.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 
 /*
- * Integer 타입의 HashSet 생성하세요
+ * 정수 타입의 HashSet 생성하세요
  * set에 숫자 3개를 추가하세요
  * set의 크기를 출력하세요
  * Iterator를 이용하여 set의 요소를 하나씩 출력하세요
@@ -18,7 +18,7 @@ public class Quiz5 {
 		hashSet.add(20);
 		hashSet.add(30);
 		
-		System.out.println(hashSet); //추가한 순서대로 저장되지 않는다
+		System.out.println(hashSet); //추가한 순서대로 저장안됨
 		System.out.println("Set의 크기: " + hashSet.size());
 		
 		//Iterator를 사용해서 set을 순회
@@ -28,7 +28,7 @@ public class Quiz5 {
 			System.out.println(i);
 		}
 		
-		//포문을 사용해서 set을 순회
+		//람다식포문을 사용해서 set을 순회
 		for(int i : hashSet) {
 			System.out.println(i);
 		}
