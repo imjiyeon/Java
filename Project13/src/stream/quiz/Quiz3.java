559e2d1324d117101672e636d2cff35c7081515c
geoblo <geoblo@naver.com>
2024-05-10 12:03:39 +0900

    ㅇㅇㅇ

--- Project13/src/stream/quiz/Quiz3.java
+++ Project13/src/stream/quiz/Quiz3.java
@@ -5,6 +5,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+// 다음과 같이 리스트를 생성하세요
+// [file1.txt, file2.pdf, file3.txt, file4.img, file5.txt, file6.img]
+// 1.파일 목록 중에서 txt 파일만 찾으세요
+// 결과: file1.txt file3.txt file5.txt
+// 2.확장자별로 파일의 개수를 구하세요
+// txt파일의 개수: 3
+// pdf파일의 개수: 1
+// img파일의 개수: 2
+// 3. 파일의 확장자만 추출하여 출력하세요
+// (hint! (.)점은 문자기호로 (\\.)으로 표현합니다 )
+// 결과: txt pdf img
+
 public class Quiz3 {
 
 	public static void main(String[] args) {
