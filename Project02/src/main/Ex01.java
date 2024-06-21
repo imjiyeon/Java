a19019e30bf72718bb0d536e4a741021d070a9df
임지연 <yoyt12@naver.com>
2024-06-21 09:54:31 +0900

    ㅇㅇ

--- Project02/src/main/Ex01.java
+++ Project02/src/main/Ex01.java
@@ -3,19 +3,19 @@ package main;
 /*
  * 변수 선언 및 사용
  * */
-//주석 단축키 - 블럭 선택 > 컨트롤 + /
-//주석 단축키 - 블럭 선택 > 컨트롤 + 시프트 + /
+//주석 단축키: 블럭 > ctrl + /
+//주석 단축키: 블럭 > ctrl + shift + /
 public class Ex01 {
 	public static void main(String[] args) {
+		
 		int x; // 정수형 변수 선언
 		x = 10; // x에 10을 대입
 
-		System.out.println(x); // x를 출력하면 변수에 담긴 값이 출력됨
-		System.out.println("안녕하세요"); // println함수는 출력후 줄바꿈이 실행됨
-		System.out.print("반갑습니다"); // print함수는 줄바꿈안함
+		System.out.println(x); // 변수에 담긴 값이 출력됨
+		System.out.println("안녕하세요"); // println 함수는 출력 후 줄바꿈이 실행됨
+		System.out.print("반갑습니다"); // print 함수는 줄바꿈안함
 		System.out.print("반갑습니다");
 
-		// sysout 작성 + 컨트롤키 + 스페이스키 
-		// -> 프린트 함수가 자동완성됨
+		// "sysout" 작성 > ctrl + space > 프린트 함수가 자동으로 완성됨
 	}
 }
