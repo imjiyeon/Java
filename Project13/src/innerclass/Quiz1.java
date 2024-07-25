a0caa8308c4aba9fc44234439a33b978d9d8aa38
geoblo <geoblo@naver.com>
2024-07-25 12:42:55 +0900

    ㅇㅇㅇ

--- Project13/src/innerclass/Quiz1.java
+++ Project13/src/innerclass/Quiz1.java
@@ -19,6 +19,11 @@ public class Quiz1 {
 		frame.setBounds(500, 300, 500, 300);
 		frame.setVisible(true);
 
+		// 키 이벤트 등록하기!
+		// 프레임을 열고, 키를 누르면 -> "키를 눌렀습니다!" 출력..
+		// 1. 구현클래스 사용해서 등록 2. 익명클래스를 사용해서 등록
+//		frame.addKeyListener(null);
+
 		// 미리 구현한 클래스로 키이벤트 등록하기
 		KeyAdapter adapter = new MyKeyListener();
 		frame.addKeyListener(adapter);
